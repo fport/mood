b98ebf6d5c41a42dfcbae116d7353a2d5e27374c
fport <furkanportakalx@gmail.com>
2024-02-25 23:09:23 +0300

    Add OpenAI model and analyzing function in utils/ai
    This commit introduces the use of OpenAI's 'gpt-3.5-turbo' Language Model in the platform and creates a new 'analyze' function in utils/ai.ts for data analysis. It also has dependencies update including addition of 'langchain' and 'zod' packages.

--- utils/ai.ts
+++ utils/ai.ts
@@ -5,7 +5,34 @@
   Transformers: https://www.youtube.com/watch?v=5vcj8kSwBCY
  */
 
-import {OpenAI} from "@langchain/openai"
+import { z } from 'zod'
+import { OpenAI } from '@langchain/openai'
+import { StructuredOutputParser } from 'langchain/output_parsers'
+
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    mood: z
+      .string()
+      .describe('the mood of the person who wrote the journal entry.'),
+    subject: z.string().describe('the subject of the journal entry.'),
+    negative: z
+      .boolean()
+      .describe(
+        'is the journal entry negative? (i.e. does it contain negative emotions?).'
+      ),
+    summary: z.string().describe('quick summary of the entire entry.'),
+    color: z
+      .string()
+      .describe(
+        'a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
+      ),
+    sentimentScore: z
+      .number()
+      .describe(
+        'sentiment of the text and rated on a scale from -10 to 10, where -10 is extremely negative, 0 is neutral, and 10 is extremely positive.'
+      )
+  })
+)
 
 const getOpenAIModel = () => {
   return new OpenAI({
@@ -18,5 +45,5 @@ const getOpenAIModel = () => {
 export const analyze = async (propmt: string) => {
   const model = getOpenAIModel()
   const result = await model.call(propmt)
-  console.log({ result });
+  console.log({ result })
 }

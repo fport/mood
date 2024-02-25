8b2d2e404ebe2317bf5da12772677367ffe40e67
fport <furkanportakalx@gmail.com>
2024-02-25 15:52:47 +0300

    Implement user authentication and data retrieval for JournalPage
    This commit introduces the getUserFromClerkID function to facilitate user authentication. It also adds a new function, getEntries, that fetches the user's journal entries from the database. The user's default page after login is also updated to 'JournalPage' from 'Dashboard'.

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,4 +1,22 @@
-export default function JournalPage() {
+import { getUserFromClerkID } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+async function getEntries() {
+  const user = await getUserFromClerkID()
+  const entries = await prisma.cvEntry.findMany({
+    where: {
+      userId: user.id
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+  return entries
+}
+
+export default async function JournalPage() {
+  const entries = await getEntries()
+  console.log('osman', entries)
   return (
     <div>JournalPage</div>
   )
---
+++ utils/auth.ts
@@ -0,0 +1,14 @@
+// import type { User } from '@clerk/nextjs/api'
+import { prisma } from './db'
+import { auth } from '@clerk/nextjs'
+
+export const getUserFromClerkID = async () => {
+  const { userId } = auth()
+  const user = await prisma.user.findUniqueOrThrow({
+    where: {
+      clerkId: userId as string,
+    }
+  })
+
+  return user
+}

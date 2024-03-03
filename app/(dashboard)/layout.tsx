3d45405572cc5f72aa9e3af3ca35d05798ae9899
fport <furkanportakalx@gmail.com>
2024-03-03 15:08:35 +0300

    Update copy and add navigation links
    The copy on the homepage and dashboard page has been updated to better reflect the purpose of the app. Also, a navigation sidebar with links to the 'Home' and 'Journal' pages has been added to the dashboard page layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,25 @@
 import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/journal', label: 'Journal' }
+]
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="h-screen w-screen relative">
       <aside className="absolute top-0 left-0 h-full w-[200px] border-r border-black/10">
-        Check Resume
+        <div>Mood 🌝</div>
+        <ul>
+          {
+            links.map(link => (
+              <li key={link.href} className="px-2 py-6 text-xl">
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))
+          }
+        </ul>
       </aside>
       <div className="ml-[200px] h-full">
         <header className="h-[60px] border-b border-black/10">
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[780px] text-center">
         <h1 className="text-6xl mb-4">🐨</h1>
-        <h1 className="text-6xl">Check Resume</h1>
+        <h1 className="text-6xl">Mood</h1>
         <Link href={href}>
           <button className="mt-8 bg-white text-black px-4 py-2 rounded-md">Get Started</button>
         </Link>

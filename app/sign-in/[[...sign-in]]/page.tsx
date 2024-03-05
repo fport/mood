b7242e213be4cfb86be2c335a4e1f14285de5796
fport <furkanportakalx@gmail.com>
2024-03-05 15:23:25 +0300

    Update sign-up and sign-in components properties
    Updated the properties in the sign-up and sign-in components in our application. The sign-in component now includes a URL for sign-up, and the sign-up component has added properties related to routing, sign-in URL, and redirection.

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,6 +2,6 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return <div className="flex w-screen h-screen items-center justify-center bg-black">
-    <SignIn />
+    <SignIn signUpUrl="/sign-up" />
   </div>
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,6 +2,12 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
   return <div className="flex w-screen h-screen items-center justify-center bg-black">
-    <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user" />
+    <SignUp
+      path="/sign-up"
+      routing="path"
+      signInUrl="/sign-in"
+      redirectUrl="/new-user"
+      afterSignUpUrl="/new-user"
+    />
   </div>
 }
